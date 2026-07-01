08e7ee1c8ed5144f9df25817e3e9823a8b61ad1a
SeoJin-L-ee <jinycap7@gmail.com>
2026-07-02 00:57:13 +0900

    26.07.02: Solved JUNGOL_1726

--- src/src/main/java/org/jungol/Main_1726.java
+++ src/src/main/java/org/jungol/Main_1726.java
@@ -19,5 +19,22 @@ public class Main_1726 {
         for (int i = N-1; i >= 1; i--) tree[i] = Math.max(tree[2*i], tree[2*i+1]);
 
         for (int i = 0; i < Q; i++) {
-            st = new StringTokenizer(br.read
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken())-1 + N;
+            int b = Integer.parseInt(st.nextToken())-1 + N;
+            int max = 0;
+
+            while (a <= b) {
+                // 왼쪽에서 오는 a는 짝수여야 묶일 수 있음
+                if (a%2 == 1) max = Math.max(max, tree[a++]);
+                // 오른쪽에서 오는 b는 홀수여야 묶일 수 있음
+                if (b%2 == 0) max = Math.max(max, tree[b--]);
+                // 안 묶이는 경우와 묶이는 경우 모두 부모 노드로 이동
+                a /= 2;
+                b /= 2;
+            }
+            sb.append(max).append("\n");
+        }
+        System.out.println(sb);
+    }
 }

633996d255bf1008b474f9d5266ac44ea4e1a9ec
SeoJin-L-ee <jinycap7@gmail.com>
2026-07-10 14:40:48 +0900

    26.07.10: Solved BOJ_java_1167

--- src/src/main/java/org/BOJ/java/Main_1167.java
+++ src/src/main/java/org/BOJ/java/Main_1167.java
@@ -1,5 +1,66 @@
 package org.BOJ.java;
 
-public class Main_1167 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
+public class Main_1167 {
+    static ArrayList<int[]>[] tree;
+    static boolean[] visited;
+    static int maxDist = 0; static int deepNode;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int V = Integer.parseInt(br.readLine());
+        tree = new ArrayList[V+1];
+        for (int i = 1; i < V+1; i++) tree[i] = new ArrayList<>();
+
+        for (int i = 0; i < V; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int cur = Integer.parseInt(st.nextToken());
+            while (true) {
+                int temp = Integer.parseInt(st.nextToken());
+                if (temp == -1) break;
+                int dist = Integer.parseInt(st.nextToken());
+                tree[cur].add(new int[] {temp, dist});
+            }
+        }
+        bfs(1, V);
+        maxDist = 0;
+        bfs(deepNode, V);
+
+        System.out.println(maxDist);
+    }
+
+    static void bfs(int start, int V) {
+        visited = new boolean[V + 1];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {start, 0});
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int curNode = cur[0];
+            int curDist = cur[1];
+
+            if (curDist > maxDist) {
+                maxDist = curDist;
+                deepNode = curNode;
+            }
+            for (int[] next : tree[curNode]) {
+                int nextNode = next[0];
+                int nextDist = next[1];
+
+                if (!visited[nextNode]) {
+                    visited[nextNode] = true;
+                    queue.offer(new int[] {nextNode, curDist+nextDist});
+                }
+            }
+        }
+    }
 }
